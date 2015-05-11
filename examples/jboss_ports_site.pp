6ac3ef97a788b77bbe15e22f9b1e2e0e6a0e1526
teleivo <mrteleivo@gmail.com>
2015-05-11 09:11:44 +0200

    improve example manifests

--- examples/jboss_ports_site.pp
+++ examples/jboss_ports_site.pp
@@ -1,12 +1,15 @@
 Exec {
-    path => [ "/usr/bin", "/bin", "/usr/sbin", "/sbin" ]
+    path => [ '/usr/bin', '/bin', '/usr/sbin', '/sbin' ]
 }
+
+package { [ 'unzip', 'curl' ]: }
 
 include 'mysql::server'
 
 class { 'dcm4chee':
-    java_path                => "/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java",
+    java_path                => '/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java',
     jboss_http_port          => 8081,
     jboss_ajp_connector_port => 8089,
+    require                  => [ Package['unzip'], Package['curl'] ],
 }
 
