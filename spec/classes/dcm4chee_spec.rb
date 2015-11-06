4226057789f225b3a3fca8bb69197139ca0145c4
teleivo <mrteleivo@gmail.com>
2015-11-06 13:14:58 +0100

    rewrite tests for incompatible os

--- spec/classes/dcm4chee_spec.rb
+++ spec/classes/dcm4chee_spec.rb
@@ -122,35 +122,30 @@ describe 'dcm4chee', :type => :class do
   end
 
   context 'should gracefully fail on any OS other than Ubuntu 14.04 64bit' do
-    describe 'on Ubuntu 14.04 32bit' do
-      let(:facts) {{
+    [
+      {
         :osfamily               => 'Debian',
         :operatingsystem        => 'Ubuntu',
         :operatingsystemrelease => '14.04',
         :architecture           => 'x86',
-      }}
-      it { is_expected.to compile.and_raise_error(/Module only supports Ubuntu 14.04 64bit/) }
-    end
-
-    describe 'on Ubuntu 12.02 64bit' do
-      let(:facts) {{
+      },
+      {
         :osfamily               => 'Debian',
         :operatingsystem        => 'Ubuntu',
         :operatingsystemrelease => '12.02',
         :architecture           => 'amd64',
-      }}
-      it { is_expected.to compile.and_raise_error(/Module only supports Ubuntu 14.04 64bit/) }
-    end
-
-    describe 'on Centos 7' do
-      let(:facts) {{
+      },
+      {
         :osfamily               => 'RedHat',
         :operatingsystem        => 'CentOS',
         :operatingsystemrelease => '7',
         :architecture           => 'amd64',
-      }}
-      it { is_expected.to compile.and_raise_error(/Module only supports Ubuntu 14.04 64bit/) }
+      },
+    ].each do |facts|
+      let(:facts) { facts }
+      it "should fail on #{facts[:operatingsystem]} #{facts[:operatingsystemrelease]} #{facts[:architecture]}" do
+        is_expected.to compile.and_raise_error(/Module only supports Ubuntu 14.04 64bit/)
+      end
     end
   end
-
 end
