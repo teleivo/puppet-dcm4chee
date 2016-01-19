7374905ec0acbb1d877afd1369d78bcfc5c34f6c
teleivo <mrteleivo@gmail.com>
2016-01-19 13:29:00 +0100

    replace config::jboss params with top-level scope vars

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,6 @@
 # Class: dcm4chee::config: See README.md for documentation.
 class dcm4chee::config () {
-  class { 'dcm4chee::config::mysql':
-  } ->
-  class { 'dcm4chee::config::jboss':
-    jboss_http_port          => $::dcm4chee::jboss_http_port,
-    jboss_ajp_connector_port => $::dcm4chee::jboss_ajp_connector_port,
-    jboss_java_opts          => $::dcm4chee::jboss_java_opts,
-  } ->
+  class { 'dcm4chee::config::mysql': } ->
+  class { 'dcm4chee::config::jboss': } ->
   class { 'dcm4chee::config::weasis': }
 }
--- manifests/config/jboss.pp
+++ manifests/config/jboss.pp
@@ -1,7 +1,10 @@
-class dcm4chee::config::jboss (
-  $jboss_http_port,
-  $jboss_ajp_connector_port,
-  $jboss_java_opts,) {
+# Class: dcm4chee::config::jboss. See README.md for documentation.
+class dcm4chee::config::jboss () {
+  
+  $jboss_http_port = $::dcm4chee::jboss_http_port
+  $jboss_ajp_connector_port = $::dcm4chee::jboss_ajp_connector_port
+  $jboss_java_opts = $::dcm4chee::jboss_java_opts
+
   $jboss_server_web_deploy_path = "${::dcm4chee::server_deploy_path}jboss-web.deployer/"
   $jboss_server_xml_path = "${jboss_server_web_deploy_path}server.xml"
   $jboss_run_conf_path = "${::dcm4chee::bin_path}run.conf"
