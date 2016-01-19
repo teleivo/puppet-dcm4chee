d1b7516f9ffcdf50f8462ffc97db8666b1fe25db
teleivo <mrteleivo@gmail.com>
2016-01-19 16:26:10 +0100

    adjust some vars to top-level scope notation

--- manifests/staging/weasis.pp
+++ manifests/staging/weasis.pp
@@ -14,12 +14,12 @@ class dcm4chee::staging::weasis (
   $weasis_dcm4chee_web_source_url =
   "${weasis_connector_base_url}${weasis_dcm4chee_web_name}"
 
-  $source_directory = "${dcm4chee::staging_path}weasis/"
+  $source_directory = "${::dcm4chee::staging_path}weasis/"
 
   file { $source_directory:
     ensure => directory,
-    owner  => $dcm4chee::user,
-    group  => $dcm4chee::user,
+    owner  => $::dcm4chee::user,
+    group  => $::dcm4chee::user,
   }
 
   staging::file { $weasis_archive_name:
@@ -30,8 +30,8 @@ class dcm4chee::staging::weasis (
 
   file { "${::dcm4chee::staging::dcm4chee_deploy_path}${weasis_archive_name}":
     ensure  => present,
-    owner   => $dcm4chee::user,
-    group   => $dcm4chee::user,
+    owner   => $::dcm4chee::user,
+    group   => $::dcm4chee::user,
     source  => "${source_directory}${weasis_archive_name}",
     require => Staging::File[$weasis_archive_name],
   }
@@ -42,11 +42,10 @@ class dcm4chee::staging::weasis (
     require => File[$source_directory],
   }
 
-  file {
-  "${dcm4chee::staging::dcm4chee_deploy_path}${weasis_connector_archive_name}":
+  file { "${::dcm4chee::staging::dcm4chee_deploy_path}${weasis_connector_archive_name}":
     ensure  => present,
-    owner   => $dcm4chee::user,
-    group   => $dcm4chee::user,
+    owner   => $::dcm4chee::user,
+    group   => $::dcm4chee::user,
     source  => "${source_directory}${weasis_connector_archive_name}",
     require => Staging::File[$weasis_connector_archive_name],
   }
@@ -57,11 +56,10 @@ class dcm4chee::staging::weasis (
     require => File[$source_directory],
   }
 
-  file { "${dcm4chee::staging::dcm4chee_deploy_path}${weasis_dcm4chee_web_name}"
-  :
+  file { "${::dcm4chee::staging::dcm4chee_deploy_path}${weasis_dcm4chee_web_name}":
     ensure  => present,
-    owner   => $dcm4chee::user,
-    group   => $dcm4chee::user,
+    owner   => $::dcm4chee::user,
+    group   => $::dcm4chee::user,
     source  => "${source_directory}${weasis_dcm4chee_web_name}",
     require => Staging::File[$weasis_dcm4chee_web_name],
   }
