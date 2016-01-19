a2f2e22005b97db9a4f692c9147c9ff929cb8190
teleivo <mrteleivo@gmail.com>
2016-01-19 10:39:48 +0100

    mysql config params overrides top level params
    * since not all params where passed on to config::mysql
    host was wrong if set to something else than default localhost
    * use top level parameters to render db connection settings

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,9 +1,6 @@
 # Class: dcm4chee::config: See README.md for documentation.
 class dcm4chee::config () {
   class { 'dcm4chee::config::mysql':
-    database_name           => $::dcm4chee::database_name,
-    database_owner          => $::dcm4chee::database_owner,
-    database_owner_password => $::dcm4chee::database_owner_password,
   } ->
   class { 'dcm4chee::config::jboss':
     jboss_http_port          => $::dcm4chee::jboss_http_port,
