01fe29f53ea9e5526375b47cd0fb91f959cefba9
teleivo <mrteleivo@gmail.com>
2016-02-04 16:33:16 +0100

    use anchors in config

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,11 +3,13 @@ class dcm4chee::config () {
   
   $database_class = "::dcm4chee::config::${dcm4chee::database_type}"
   class { $database_class: } ->
-  class { '::dcm4chee::config::jboss': }
+  class { '::dcm4chee::config::jboss': } ->
+  anchor { '::dcm4chee::config_end': }
   
   if $::dcm4chee::weasis {
     class { '::dcm4chee::config::weasis': }
     Class['::dcm4chee::config::jboss'] ->
-    Class['::dcm4chee::config::weasis']
+    Class['::dcm4chee::config::weasis'] ->
+    Anchor['::dcm4chee::config_end']
   }
 }
